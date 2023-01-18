7bdde9b8b7bb5ba48585bcf3c3cb287587712c32
mipdableep <idodo.talmor@gmail.com>
2023-01-18 13:46:14 +0200

    organization of chroot and emulator

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,42 @@
-#opens vs-code at the raspberry pi home folder
-alias codePi='code /media/fares/rootfs/home/pi/'
-#open ssh with the argumant being the ip of the pi
-alias sshfspi="sudo sshfs -o allow_other pi@$1:/home/pi/ /media/fares/sshTest"
-#cd to pi home dir
-alias cdtopi='cd /media/fares/rootfs/home/pi/'
+#costum
+    
+    remove_cmake_make(){
+        if [[ $(ls . | grep ".cmake") ]]
+        then
+            echo -e "\ndeleating dir\n"
+            rm -rf *
+            cmake .. 
+            make -j
+        else
+            echo "no cmake to delete"
+        fi
+        }
+
+
+
+    #project arucoSwarm
+    alias codePiAruco='code /media/fares/rootfs/home/pi/arucoSwarm/'
+    alias runserver='python3 /home/fares/rbd/projects/aruco_swarm/arucoSwarm/server.py 0.0.0.0 9090 $1'
+
+    alias copyexe="cp followAruco /home/fares/rbd/projects/aruco_swarm/"
+    alias runsshServer="cd /home/fares/rbd/tools/rpi-Tools/ssh_automation && python3 sshServer.py $1"
+
+    #system
+    alias n='nautilus -w . &'
+    alias getip='hostname -I | cut -d " " -f 1'
+    alias gfp='git fetch && git pull'
+    alias rcm=remove_cmake_make
+    alias umountall='sudo umount /media/fares/*'
+    alias copy='xclip -selection clipboard'
+    alias pwdc='pwd | copy'
+    alias py='python3'
+    alias cp_progress='rsync -ah --progress'
+    alias rcmc='echo "*rm -rf * && cmake .. && make -j" | copy'
+
+    #rpi
+    alias cdtopi='cd /media/fares/rootfs/home/pi/'
+    alias rpipinout='eog /home/fares/rbd/tools/hardwere &'
+
+    #bashrc related
+    alias runbashrc='. ~/.bashrc'
+    alias codebashrc='code /home/fares/.bashrc'
---
+++ chroot/internal_rpi_emu.md
@@ -0,0 +1,6 @@
+run the next command to mount the img
+```
+gnome-disk-image-mounter -w <img/path>
+```
+
+then chroot as usual
