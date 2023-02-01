bafd06b86f2896ce57331ff5289b72c274ba4862
mipdableep <idodo.talmor@gmail.com>
2023-02-01 16:23:12 +0200

    folder to connect to sshServer on boot

---
+++ ssh_automation/sshServer_boot/bashrc_extension.sh
@@ -0,0 +1,9 @@
+if [[ "$(awk '$5=="/" {print $1}' </proc/1/mountinfo)" != "$(awk '$5=="/" {print $1}' </proc/$$/mountinfo)" ]]
+then
+    echo -e "\nin chroot, breaking\n"
+else
+
+    while ! [[ $(ip a | grep "inet 192") ]]; do sleep 1s; done
+    
+    python3 /home/pi/sshServer_boot/sshClient.py    
+fi
