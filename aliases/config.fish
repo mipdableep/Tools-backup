205669dd8df517e7c7ce045131ba9d288a113f28
Ido Talmor <ifw.talmor@gmail.com>
2023-09-26 03:00:30 +0300

    cleanup, added install scripts
    moved camera calibration and removed things that are backed in conf_files

--- aliases/config.fish
+++
@@ -1,8 +0,0 @@
-source /home/ido/Tools/Tools-backup/aliases/aliases.fish
-set PATH "/home/ido/.cargo/bin:$PATH"
-set PATH "/home/ido/.local/share/bob/nvim-bin:$PATH"
-set -x LC_CTYPE en_US.UTF-8
-set -g fish_greeting ''
-setxkbmap -option caps:escape
-source /usr/share/doc/fzf/examples/key-bindings.fish
-fzf_key_bindings
---
+++ install_scripts/reinstall_fish.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function ask_yes_or_no {
+    while true; do
+        read -p "$1 (y/n) " yn
+        case $yn in
+            [Yy]* ) return 0;;
+            [Nn]* ) return 1;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+
+# Given Ubuntu:
+
+if ask_yes_or_no "Install fish shell?"; then
+    sudo apt-get install fish -y
+fi
+
+# Set fish as default shell
+if ask_yes_or_no "Set fish as the default shell?"; then
+    echo /usr/bin/fish | sudo tee -a /etc/shells
+    chsh -s /usr/bin/fish
+fi
