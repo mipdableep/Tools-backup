598042db17e27d6ce2b702b3a733b96c84d8ec8a
Ido Talmor <ifw.talmor@gmail.com>
2023-11-09 20:46:56 +0200

    calib changes

--- camera_calibration/tello_frame_collection/tello_frame_collection.py
+++ camera_calibration/tello_frame_collection/tello_frame_collection.py
@@ -1,39 +1,41 @@
 import cv2
 from time import sleep
-import djitellopy
 import VCS
 
 #! params:
 # rate photos are saved (one out of ...)
-save_rate = 10
-save_path = "/home/fares/rbd/tools/Tools-backups/camera calibration/tello_frame_collection/images/"
-cap_string = "udp://0.0.0.0:11111?overrun_nonfatal=1&fifo_size=5000"
-tello = True
+SAVE_RATE = 10
+SAVE_PATH = "/home/ido/Tools/Tools-backup/camera_calibration/calib_from_img_dir/imgs/"
+# cap_string = "udp://0.0.0.0:11111?overrun_nonfatal=1&fifo_size=5000"
+CAP_STRING = 0
+USE_TELLO = False
+
+if USE_TELLO:
+    import djitellopy
 
 
 def main():
-    if tello:
+    if USE_TELLO:
         t1 = djitellopy.Tello()
         t1.connect()
         t1.streamon()
-        
+
     # Create a VideoCapture object and read from input file
-    cap = VCS.VideoCapture(cap_string)
+    cap = cv2.VideoCapture(CAP_STRING)
     # Check if camera opened successfully
-    
+
     counter = 0
     name_counter = 0
-    
-    # Read until video is completed 
-    while (cap._live):
+
+    # Read until video is completed
+    while cap.isOpened:
         # Capture frame-by-frame
         ret, frame = cap.read()
         if ret == True:
+            cv2.imshow("Frame", frame)
 
-            cv2.imshow('Frame', frame)
-
-            if counter % save_rate == 0:
-                frame_name = "" + save_path + str(name_counter) + ".jpg"
+            if counter % SAVE_RATE == 0:
+                frame_name = "" + SAVE_PATH + str(name_counter) + ".jpg"
                 print(frame_name)
                 cv2.imwrite(frame_name, frame)
                 name_counter += 1
@@ -41,7 +43,7 @@ def main():
             counter += 1
 
             # Press Q on keyboard to  exit
-            if cv2.waitKey(25) & 0xFF == ord('q'):
+            if cv2.waitKey(25) & 0xFF == ord("q"):
                 break
 
         else:
@@ -52,6 +54,7 @@ def main():
 
     # Closes all the frames
     cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
