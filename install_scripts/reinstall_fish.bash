718c5696cb0efc2c4f5573b18f44bb95b991179f
Ido Talmor <ifw.talmor@gmail.com>
2023-12-30 01:04:36 +0200

    install scripts rewrite

--- install_scripts/reinstall_fish.bash
+++ install_scripts/reinstall_fish.bash
@@ -1,24 +1,8 @@
 #!/bin/bash
 
-function ask_yes_or_no {
-    while true; do
-        read -p "$1 (y/n) " yn
-        case $yn in
-            [Yy]* ) return 0;;
-            [Nn]* ) return 1;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
-}
-
 # Given Ubuntu:
-
-if ask_yes_or_no "Install fish shell?"; then
-    sudo nala install fish -y
-fi
+sudo nala install fish -y
 
 # Set fish as default shell
-if ask_yes_or_no "Set fish as the default shell?"; then
-    echo /usr/bin/fish | sudo tee -a /etc/shells
-    chsh -s /usr/bin/fish
-fi
+echo /usr/bin/fish | sudo tee -a /etc/shells
+chsh -s /usr/bin/fish
---
+++ install_scripts/reinstall_fish_plugins.bash
@@ -0,0 +1,10 @@
+#! /usr/bin/fish
+
+#  Enter Fish and install Fisher and plugins
+fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
+# Install Lambda theme
+fish -c "fisher install netologist/theme-lambda"
+# Install z (improved cd)
+fish -c "fisher install jethrokuan/z"
+# Install fish nvm
+fish -c "fisher install jorgebucaran/nvm.fish"
